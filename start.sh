0ee2db73cfe992968e39a0f331b9ab72d0ad917a
bsefwe <92920899+bsefwe@users.noreply.github.com>
2022-04-16 09:37:58 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 UUID=5aaed9b7-7fe3-47c3-bb52-db59859ce198
-WebPage=https://www.bing.com
+WebPage=https://www.microsoft.com/en-us/
 CaddyConfig=https://raw.githubusercontent.com/bsefwe/Rail/master/etc/Caddyfile
 XRayConfig=https://raw.githubusercontent.com/bsefwe/Rail/master/etc/xray.json
 Xray_Newv=`wget --no-check-certificate -qO- https://api.github.com/repos/XTLS/Xray-core/tags | grep 'name' | cut -d\" -f4 | head -1 | cut -b 2-`
